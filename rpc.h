986ca4c54cd4338e75ab8e540af946943bc5b10c
ClaraRuna <runa.s@gmx.net>
2017-06-19 12:57:27 +0200

    new members in rpc_Header

--- rpc.h
+++ rpc.h
@@ -3,12 +3,27 @@
 
 #define MAX_BUFFER 1024
 
+typedef struct UUID{ //16 Byte
+	//TODO
+	
+	unsigned char * toBuffer();
+}
+
 typedef struct rpc_Header{
 	u_char version;
 	u_char packetType; //Request, Response, Ping, etc
 	u_char flags1;
 	u_char flags2;
 	u_char dRep[3]; //Representation of datatypes 
+	u_char serialHigh; //Das höherwertige Byte der Fragmentnr des Aufrufs?
+	//UUIDs je 16 Byte
+	struct UUID objectUUID; 		
+	struct UUID interfaceUUID;		
+	struct UUID activityUUID; 		
+	u_char serverBootTime[4]; 	//req -> 0 ; resp -> zeit
+	u_char interfaceVersion[4];	//=1
+	u_char sequenceNumber[4];	//
+	
 	
 
 	
@@ -20,6 +35,7 @@ typedef struct rpc_Header{
 		dRep[0]=0b11111111; //Encoding ENBCD & Little Endian
 		dRep[1]=0x00; //IEEE Floating Point
 		dRep[2]=0x00; //keine Bedeutung, sollen 0 sein
+		//TODO: andere Felder setzen
 	}
 	
 	unsigned char *  toBuffer();
